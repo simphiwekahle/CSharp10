7592642407dfa5ad811e3bea604811ba5b28246d
SphepheloN <SphepheloN@ZAZN-BET-0B725B>
2024-01-09 12:04:09 +0200

    Getting Started with C# 10 and .NET 6+

--- CSharp10/Program.cs
+++ CSharp10/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// C# is the main language for .NET development
+// C# is an Object-oriented and a type-safe prgramming language
+
+Console.WriteLine("Hello everybody");
+Console.WriteLine("Please enter your name: ");
+
+string name = Console.ReadLine();
+Console.WriteLine($"Hello {name}");
